11259c3025086a9801a0ab2d9755171a88769c18
mwawrzyniak <mwawrzyniak@unistra.fr>
2023-11-04 11:19:19 +0100

    fix: delete ? in url params

--- src/services/actionloaders/search_game.js
+++ src/services/actionloaders/search_game.js
@@ -5,7 +5,7 @@ export const searchGameActionLoader = async ({ request }) => {
   const url = new URL(request.url);
   const search = url.searchParams.get("search");
   if (search) {
-    return redirect("/category/most-popular?" + url.searchParams);
+    return redirect("/category/most-popular" + url.searchParams);
   }
   return null;
 };
