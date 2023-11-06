9b901a798bda0119a945da5a459ef5f7da1b1608
mwawrzyniak <mwawrzyniak@unistra.fr>
2023-11-06 19:27:19 +0100

    fix: use switch instead of if else

--- src/services/actionloaders/category_games.js
+++ src/services/actionloaders/category_games.js
@@ -9,7 +9,7 @@ import {
   get_game_by_name_by_sort,
   get_most_popular_games,
   get_genre_games,
-  get_most_popular_games_by_sort
+  get_most_popular_games_by_sort,
 } from "../api/games";
 
 const LIMIT = 30000;
@@ -26,27 +26,24 @@ export const categoryGamesActionLoader = async ({ request, params }) => {
   if (!isNaN(params.category)) {
     return await get_genre_games(params.category, params.sort);
   }
-  if (!params.sort) {
-    if (params.category === "free-to-play") {
-      return await get_free_games(LIMIT);
-    } else if (params.category === "most-recent") {
+  switch (params.category) {
+    case "free-to-play":
+      return !params.sort
+        ? await get_free_games(LIMIT)
+        : await get_free_games_by_sort(LIMIT, params.sort);
+    case "most-recent":
+      return !params.sort
+        ? await get_most_recents_games(LIMIT)
+        : await get_most_recent_by_sort(LIMIT, params.sort);
+    case "most-popular":
+      return !params.sort
+        ? await get_most_popular_games(LIMIT)
+        : await get_most_popular_games_by_sort(LIMIT, params.sort);
+    case "top-deals":
+      return !params.sort
+        ? await get_top_deals(LIMIT)
+        : await get_top_deals_by_sort(LIMIT, params.sort);
+    default:
       return await get_most_recents_games(LIMIT);
-    } else if (params.category === "most-popular") {
-      return await get_most_popular_games(LIMIT);
-    } else if (params.category === "top-deals") {
-      return await get_top_deals(LIMIT);
-    }
-  } else {
-    if (params.category === "top-deals") {
-      return await get_top_deals_by_sort(LIMIT, params.sort);
-    } else if (params.category === "most-recent") {
-      return await get_most_recent_by_sort(LIMIT, params.sort);
-    } else if (params.category === "free-to-play") {
-      return await get_free_games_by_sort(LIMIT, params.sort);
-    } else if (params.category === "most-popular") {
-      return await get_most_popular_games_by_sort(LIMIT, params.sort);
-    } else {
-      return await get_most_recents_games(LIMIT);
-    }
   }
 };
