180074ee2c242a014d41262ed73607be0f6b197e
mwawrzyniak <mwawrzyniak@unistra.fr>
2023-11-25 11:22:14 +0100

    refactor: change variable name in api config and add fallback api

--- src/services/api/config.js
+++ src/services/api/config.js
@@ -1,71 +1,73 @@
-const $url_host = "http://gamedealhub.net/API_TOKEN=hvbPup569HAwx-byI2kNc1zLD-6=37DAym/";
+const url_host = "http://gamedealhub.net/API_TOKEN=hvbPup569HAwx-byI2kNc1zLD-6=37DAym/";
+// Api de secours
+// const url_host = "https://api.dev.gamedealhub.com/API_TOKEN=hvbPup569HAwx-byI2kNc1zLD-6=37DAym/";
 
 export const urlGetFreeGames = (limit) => {
-    return $url_host + "api/games/freetoplay/" + limit;
+    return url_host + "api/games/freetoplay/" + limit;
 }
 
 export const urlGetMostRecentsGames = (limit) => {
-    return $url_host + "api/games/mostrecents/" + limit;
+    return url_host + "api/games/mostrecents/" + limit;
 }
 
 export const urlGetUnderPriceGames = (price) => {
-    return $url_host + "api/games/underprice/" + price;
+    return url_host + "api/games/underprice/" + price;
 }
 
 export const urlGetTopDealsGames = (limit) => {
-    return $url_host + "api/games/topdeals/" + limit;
+    return url_host + "api/games/topdeals/" + limit;
 }
 
 export const urlGetTopDealsGamesBySort = (limit, sort) => {
-    return $url_host + "api/games/topdeals/" + limit + "/" + sort;
+    return url_host + "api/games/topdeals/" + limit + "/" + sort;
 }
 export const urlGetFreeGamesBySort = (limit, sort) => {
-    return $url_host + "api/games/freetoplay/" + limit + "/" + sort;
+    return url_host + "api/games/freetoplay/" + limit + "/" + sort;
 }
 export const urlGetMostRecentsGamesBySort = (limit, sort) => {
-    return $url_host + "api/games/mostrecents/" + limit + "/" + sort;
+    return url_host + "api/games/mostrecents/" + limit + "/" + sort;
 }
 export const urlGetGameByName = (game_name) => {
-    return $url_host + "api/games/search/" + game_name;
+    return url_host + "api/games/search/" + game_name;
 }
 export const urlGetGameByNameBySort = (game_name, sort) => {
-    return $url_host + "api/games/search/" + game_name + "/" + sort;
+    return url_host + "api/games/search/" + game_name + "/" + sort;
 }
 export const urlGetMostPopularGames = (limit) => {
-    return $url_host + "api/games/popular/" + limit;
+    return url_host + "api/games/popular/" + limit;
 }
 export const urlGetMostPopularGamesBySort = (limit, sort) => {
-    return $url_host + "api/games/popular/" + limit + "/" + sort;
+    return url_host + "api/games/popular/" + limit + "/" + sort;
 }
 // GAME INFO
 export const urlGetGameById = (game_id) => {
-    return $url_host + "api/game/" + game_id;
+    return url_host + "api/game/" + game_id;
 }
 export const urlGetGameMedia = (game_id) => {
-    return $url_host + "api/game/media/" + game_id;
+    return url_host + "api/game/media/" + game_id;
 }
 
 // TAGS
 export const urlGetGenreGames = (genre_id, sort) => {
-    return $url_host + "api/games/" + genre_id + "/" + (sort ? sort : "");
+    return url_host + "api/games/" + genre_id + "/" + (sort ? sort : "");
 }
 
 // USERS
 export const urlSignup = () => {
-    return $url_host + "api/users/signup";
+    return url_host + "api/users/signup";
 }
 export const urlSignin = () => {
-    return $url_host + "api/users/signin";
+    return url_host + "api/users/signin";
 }
 export const urlWishlist = () => {
-    return $url_host + "api/users/wishlist";
+    return url_host + "api/users/wishlist";
 }
 export const urlGetWishlistById = () => {
-    return $url_host + "api/users/getwishlist";
+    return url_host + "api/users/getwishlist";
 }
 export const urlGetWishlistByUserId = (user_id) => {
-    return $url_host + "api/user/" + user_id + "/wishlist";
+    return url_host + "api/user/" + user_id + "/wishlist";
 }
 export const urlDeleteGameWishlistByUserId = (user_id, game_id) => {
-    return $url_host + "api/user/" + user_id + "/wishlist/" + game_id;
+    return url_host + "api/user/" + user_id + "/wishlist/" + game_id;
 }
